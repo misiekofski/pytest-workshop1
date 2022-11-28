dae76ebf4da57c5a8c44b1b53368648f7a336130
Patryk Szczepański <patryk_szczepanski@rossmann.com.pl>
2022-11-28 09:44:18 +0100

    zad1-2

--- pb/tests/test_phonebook_basics.py
+++ pb/tests/test_phonebook_basics.py
@@ -3,7 +3,31 @@ from pb.phonebook import PhoneBook
 
 def test_lookup_by_name():
     phonebook = PhoneBook()
-    phonebook.add("Rossmann", "123456789")
-    assert "123456789" == phonebook.lookup("Rossmann")
+    phonebook.add("Rossmann", "12345678")
+    assert "12345678" == phonebook.lookup("Rossmann")
 
+
+def test_phonebook_contains_all_names():
+    phonebook = PhoneBook()
+    phonebook.add("Rossmann", "123456789")
+    phonebook.add("Missing", "123456789")
+    assert phonebook.names() == {"Rossmann", "Missing"}
+
+
+def test_number_isnt_numeric():
+    phonebook = PhoneBook()
+    phonebook.add("Rossmann", "telefon")
+    assert "telefon" == phonebook.lookup("Rossmann")
+
+
+def test_number_too_long():
+    phonebook = PhoneBook()
+    phonebook.add("Rossmann", "123456789012")
+    assert "123456789012" == phonebook.lookup("Rossmann")
+
+
+def test_number_too_short():
+    phonebook = PhoneBook()
+    phonebook.add("Rossmann", "12345678")
+    assert "12345678" == phonebook.lookup("Rossmann")
 
