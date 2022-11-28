ec6e574c9011826aa23b8bc8d792ed68a7a6b00c
Patryk Janowski <patryk_janowski@rossmann.com.pl>
2022-11-28 10:58:53 +0100

    cleaning

--- pb/tests/test_phonebook_basics.py
+++ pb/tests/test_phonebook_basics.py
@@ -2,15 +2,14 @@ import pytest
 
 from pb.phonebook import PhoneBook
 
+
 class TestPhoneBook:
     def test_lookup_by_name(self, empty_phonebook: PhoneBook):
         empty_phonebook.add("Rossmann", "123456789")
         assert "123456789" == empty_phonebook.lookup("Rossmann")
 
-
     def test_phonebook_contains_all_names(self, populated_phonebook: PhoneBook):
         assert populated_phonebook.names() == {"Rossmann", "Missing"}
-
 
     @pytest.mark.parametrize(
         'number, result', (('12345678', 0), ('123456789012', 0))
